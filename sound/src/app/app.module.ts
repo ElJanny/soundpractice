11b0bc016579e8678249f6e2d9549da85372bd8f
Kónya János András <konya.janos.a@gmail.com>
2021-07-14 15:50:48 +0200

    landing-page+music player

--- sound/src/app/app.module.ts
+++ sound/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { MusicPlayerComponent } from './pages/landing-page/music-player/music-player.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModuelModule } from './modules/material-moduel/material-module.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    MainLayoutComponent,
+    MusicPlayerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModuelModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
