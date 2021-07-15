5d2d9f38cbe78b1895249214631c699875c34cf5
ModzsoTheBeast <lokospatrik8@gmail.com>
2021-07-15 10:52:13 +0200

    maybe good
    UwU

--- sound/src/app/app.module.ts
+++ sound/src/app/app.module.ts
@@ -3,29 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
-import { MusicPlayerComponent } from './pages/landing-page/music-player/music-player.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModuelModule } from './modules/material-moduel/material-module.module';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LandingPageComponent,
-    MainLayoutComponent,
-    MusicPlayerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModuelModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
