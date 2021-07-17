1b13fdc3f570fab339ff1fb136d5a8eabf7cff31
Kónya János András <konya.janos.a@gmail.com>
2021-07-17 14:29:34 +0200

    navbar-done

--- sound/src/app/layout/navigation-bar/navigation-bar.component.ts
+++ sound/src/app/layout/navigation-bar/navigation-bar.component.ts
@@ -9,38 +9,58 @@ import { HostListener } from '@angular/core';
 })
 export class NavigationBarComponent implements OnInit {
 
-  public scrolledTop=true;
-  public menuTitles:string[] = ["rólam","elérhetőség","galéria"]
+  public scrolledTop:boolean=true;
+  public isMobile:boolean=true;
+  public menuTitles:menuElement[] = [
+    {title: "rólam",path: "aboutme"},
+    {title:"elérhetőség",path:"contact"},
+    {title:"galéria",path: "gallery"}]
+
   constructor() { }
 
   ngOnInit(): void {
-
+    if(window.innerWidth>=1000){
+      this.isMobile= false;
+    }else{
+      this.isMobile= true;
+    }
   }
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll($event: any) {
     let element = document.querySelector('nav') as HTMLElement;
-    if(window.pageYOffset >= window.innerHeight){
-      this.scrolledTop = false;
-      this.checkScrollState();
+    if(window.innerWidth>=1000){
+      if(window.pageYOffset > window.innerHeight-10){
+        this.scrolledTop = false;
+        this.checkScrollState();
+      }else{
+        this.scrolledTop= true;
+
+      }
     }else{
       this.scrolledTop= true;
-
+      this.isMobile= true;
     }
+
   }
 
   checkScrollState(){
     if(window.pageYOffset >window.innerHeight*3){
-      this.setMenuTitles("rólam","elérhetőség","galéria");
+      this.setMenuTitles({title:"elérhetőség",path:"contact"},{title: "rólam",path: "aboutme"},{title:"galéria",path: "gallery"});
     }else if(window.pageYOffset >window.innerHeight*2){
-      this.setMenuTitles("galéria","elérhetőség","rólam");
+      this.setMenuTitles({title:"galéria",path: "gallery"},{title:"elérhetőség",path:"contact"},{title: "rólam",path: "aboutme"});
     }else if(window.pageYOffset >window.innerHeight){
-      this.setMenuTitles("elérhetőség","rólam","galéria");
+      this.setMenuTitles({title: "rólam",path: "aboutme"},{title:"elérhetőség",path:"contact"},{title:"galéria",path: "gallery"});
     }
   }
 
-  setMenuTitles(actualTitle: string, leftTitle: string, rightTitle: string){
+  setMenuTitles(actualTitle: menuElement, leftTitle: menuElement, rightTitle: menuElement){
     this.menuTitles= [actualTitle,leftTitle,rightTitle];
   }
 
+}
+
+interface menuElement{
+  title: string,
+  path: string
 }
